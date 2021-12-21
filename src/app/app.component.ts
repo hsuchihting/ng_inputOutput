6fef5a9d46e553a997e25b64f7bc827d62e35316
Tim Hsu <tim.hsu@tpisoftware.com>
2021-12-22 00:05:59 +0800

    node bug

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,17 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  //#region data
   info = [
     {
       now_node: 'T01',
-      node_action: 'OK',
+      node_action: '2',
       action_date: '2021-12-10',
       memo: 'test',
       userName: 'Tom',
     },
     {
       now_node: 'T02',
-      node_action: 'OK',
+      node_action: '2',
       action_date: '2021-12-10',
       memo: 'test',
       userName: 'Mary',
@@ -26,7 +27,7 @@ export class AppComponent {
   node = [
     {
       node: 'T01',
-      children: ['T02'],
+      children: ['T02', 'T01_1'], //* children 的第一個節點為下一個主節點的值。
     },
     {
       node: 'T02',
@@ -65,13 +66,14 @@ export class AppComponent {
       children: [''],
     },
   ];
+  //#endregion
 
   ngOnInit(): void {
     this.process();
   }
 
   nodeArr: any[] = [];
-  nodeInfo: any;
+  nodeInfo: any[] = [];
   process() {
     let node = this.node;
     let info = this.info;
@@ -81,6 +83,7 @@ export class AppComponent {
       let childrenList: { node: string; status: string }[] = [];
 
       item.children.forEach((childrenNodeName) => {
+        //*子節點
         let currentChild = info.find(
           (list) => list.now_node === childrenNodeName
         );
@@ -93,6 +96,7 @@ export class AppComponent {
         childrenList.push(childNode);
       });
 
+      //*主節點
       let currentMain = info.find((main) => main.now_node === item.node);
       let mainStatus = !!currentMain ? currentMain.node_action : undefined;
 
@@ -104,7 +108,7 @@ export class AppComponent {
       newArr.push(mainNode);
     });
     this.nodeArr = newArr;
-    this.nodeArr.pop();
+    this.nodeArr.pop(); //* 原始資料是因為結尾的 children 是空字串，在繪圖會噴錯，所以要把最後一個節點移除。
     console.log(this.nodeArr);
 
     let infoList: any[] = [];
@@ -114,13 +118,36 @@ export class AppComponent {
       info.push(item.action_date);
       let nodeName = item.now_node;
 
-      this.nodeInfo = {
+      let nodeInfo = {
         node: item.now_node,
         node_name: nodeName ? nodeName : '',
+        node_action: item.node_action,
         info: info,
         memo: item.memo ? item.memo : '',
       };
+      infoList.push(nodeInfo);
     });
-    this.nodeInfo = infoList;
+
+    //*新增主節點給 info 陣列
+    this.nodeArr.filter((item) => {
+      console.log('item', item.node);
+
+      let infoNode = infoList.find((info) => info.node === item.node);
+
+      if (item.node !== infoNode) {
+        console.log(item.node, infoNode);
+
+        let nodeInfo = {
+          node: item.node,
+          node_action: infoNode.node_action, //*T03 的 node_action 會是 undefined ，所以會噴錯
+          node_name: '',
+          info: [],
+          memo: '',
+        };
+        this.nodeInfo.push(nodeInfo);
+      }
+    });
+
+    console.log(this.nodeInfo);
   }
 }
